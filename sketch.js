5f60c4dfd0619460365f2a2a38aec7a53e7e1558
Mars Deltor <spoonybard12@gmail.com>
2016-12-26 19:09:16 -0600

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var rButtonValue = 0;
+var lButtonValue = 0;
+
+function setup() {
+  createCanvas(900, 600);
+}
+
+function draw() {
+  background(230);
+  
+  // COUNTER BOX
+  line(width/2, 0, width/2, height);
+  
+  // NUMBER DISPLAY
+  textSize(64);
+  text(lButtonValue.toString(), width/4, height/2);
+  text(rButtonValue.toString(), 3*width/4, height/2);
+}
+
+function mouseClicked() {
+  if (mouseX <= width/2) {
+    lButtonValue ++;
+  }
+  else if (mouseX > width/2){
+    rButtonValue ++;
+  }
+}
